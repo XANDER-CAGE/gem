d116728cdb6713ac016062b5f7854b704e920b94
akhmedmukhtarov <axmedmuxtarov@icloud.com>
2024-07-09 19:36:48 +0500

    feat: init module channel

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ChannelModule } from './modules/channel/channel.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), ChannelModule],
 })
 export class AppModule {}
