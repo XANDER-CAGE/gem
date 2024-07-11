1b14f32392873b5e65ac482c6e217a541360b344
akhmedmukhtarov <axmedmuxtarov@icloud.com>
2024-07-11 12:47:31 +0500

    remove: log

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,18 @@ import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './common/config/swagger.config';
 import { env } from './common/config/env.config';
 
+const api = `http://${env.API_HOST}:${env.PORT}`;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'fatal', 'warn', 'verbose'],
+  });
   if (env.isDevelopment || env.isTest) {
     const document = SwaggerModule.createDocument(app, swaggerConfig, {
       deepScanRoutes: true,
     });
     SwaggerModule.setup('swagger', app, document);
+    console.log(`Swagger -  ${api}/swagger`);
   }
   await app.listen(env.PORT || 3000);
 }
-bootstrap().then(() =>
-  console.log(`http://${env.API_HOST}:${env.PORT}/swagger`),
-);
+bootstrap().then(() => console.log('API runnning on port ', env.PORT));
