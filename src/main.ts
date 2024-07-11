b5bb077990db02e400bec5f004b65e78f838064b
akhmedmukhtarov <axmedmuxtarov@icloud.com>
2024-07-11 16:00:29 +0500

    feat: channel init

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './common/config/swagger.config';
 import { env } from './common/config/env.config';
+import { ValidationPipe } from '@nestjs/common';
 
 const api = `http://${env.API_HOST}:${env.PORT}`;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'fatal', 'warn', 'verbose'],
   });
+  app.setGlobalPrefix('api');
   if (env.isDevelopment || env.isTest) {
     const document = SwaggerModule.createDocument(app, swaggerConfig, {
       deepScanRoutes: true,
@@ -16,6 +18,7 @@ async function bootstrap() {
     SwaggerModule.setup('swagger', app, document);
     console.log(`Swagger -  ${api}/swagger`);
   }
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(env.PORT || 3000);
 }
 bootstrap().then(() => console.log('API runnning on port ', env.PORT));
