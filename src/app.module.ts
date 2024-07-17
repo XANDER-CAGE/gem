afa4eeb908e0d1e28fbf919a066849407ce78001
akhmedmukhtarov <axmedmuxtarov@icloud.com>
2024-07-17 16:11:42 +0500

    replace: transaction module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { ProductReviewsModule } from './modules/product-reviews/product-reviews.
 import { MarketCategoriesModule } from './modules/market-categories/market-categories.module';
 import { ChannelModule } from './modules/channel/channel.module';
 import { BadgeModule } from './modules/badge/badge.module';
-import { TransactionModule } from './transaction/transaction.module';
+import { TransactionModule } from './modules/transaction/transaction.module';
 import { LevelModule } from './modules/level/level.module';
 import { FullStreaksModule } from './modules/full-streaks/full-streaks.module';
 
