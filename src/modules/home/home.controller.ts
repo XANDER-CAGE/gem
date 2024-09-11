1ddf289f880708b8afbd22c7f93e120fa071e529
akhmedmukhtarov <axmedmuxtarov@icloud.com>
2024-09-11 15:04:57 +0500

    fix: connectin beetween lms and gem

--- src/modules/home/home.controller.ts
+++ src/modules/home/home.controller.ts
@@ -87,7 +87,7 @@ export class HomeController {
     return CoreApiResponse.success(null);
   }
 
-  @Cron('0 0 10 * * *')
+  @Cron('0 0 14 * * *')
   async handleGradeCron() {
     await this.attendanceService.attendanceCron();
     await this.homeService.handleGradeCron();
