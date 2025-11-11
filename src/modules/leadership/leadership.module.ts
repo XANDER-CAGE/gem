a58cf8157c6274aef101e6d9b8ce71e1bf3bf65e
Saidazam Rasulhujaev <rasulhuzaevsaid@gmail.com>
2025-11-11 13:48:58 +0500

    fix: cron

--- src/modules/leadership/leadership.module.ts
+++ src/modules/leadership/leadership.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { LeadershipService } from './leadership.service';
-import { LeadershipController } from './leadership.controller';
 import { StudentProfilesModule } from '../student-profiles/student-profiles.module';
-import { LeadershipRepo } from './repo/leadership.repo';
 import { TransactionModule } from '../transaction/transaction.module';
+import { LeadershipController } from './leadership.controller';
+import { LeadershipService } from './leadership.service';
+import { LeadershipRepo } from './repo/leadership.repo';
 
 @Module({
-  imports: [StudentProfilesModule, TransactionModule],
+  imports: [StudentProfilesModule, TransactionModule,],
   controllers: [LeadershipController],
   providers: [LeadershipService, LeadershipRepo],
   exports: [LeadershipService],
 })
-export class LeadershipModule {}
+export class LeadershipModule { }
